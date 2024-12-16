37a594836092fc3df0eb372d4d24215c6091abce
aliozanoguz <ali.oguz@hisarschool.k12.tr>
2024-12-16 08:26:08 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ali Oguz on 16.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
